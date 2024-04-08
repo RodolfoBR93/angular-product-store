8587899104df8f4a5fa8e4bc953d55e48c7408f8
Rodolfo Silva <rodolfobruno93@gmail.com>
2024-04-08 06:43:15 -0300

    refactor: define configuração da snackbar de forma global

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ValueProvider } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+const SNACK_BAR_CONFIG: ValueProvider = {
+  provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  useValue: {
+    duration: 3000,
+    horizontalPosition: 'right',
+    verticalPosition: 'top'
+  } as MatSnackBarConfig,
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimations(), 
+    provideHttpClient(),
+    SNACK_BAR_CONFIG
+
+  ]
 };
